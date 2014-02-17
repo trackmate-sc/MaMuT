e6a658c377b2b1f8abe4c72af1b5c6da1508a57f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-02-17 13:06:55 +0100

    Release v0.10.0
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/mamut/MaMuT.java
+++ src/main/java/fiji/plugin/mamut/MaMuT.java
@@ -130,7 +130,7 @@ public class MaMuT implements ModelChangeListener
 
 	public static final String PLUGIN_NAME = "MaMuT";
 
-	public static final String PLUGIN_VERSION = "0.9.0-SNAPSHOT";
+	public static final String PLUGIN_VERSION = "0.10.0";
 
 	private static final double DEFAULT_RADIUS = 10;
 
