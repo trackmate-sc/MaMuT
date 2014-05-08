9c231ffc117645e2964fa476027957945dba6ae8
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2014-05-08 11:12:30 +0200

    When importing TGMM data, also loads transforms to put the back in global coordinates.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/fiji/plugin/mamut/LoadTGMMAnnotationPlugIn.java
+++ src/main/java/fiji/plugin/mamut/LoadTGMMAnnotationPlugIn.java
@@ -1,10 +1,5 @@
 package fiji.plugin.mamut;
 
-import ij.IJ;
-import ij.ImageJ;
-
-import java.io.File;
-
 import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Settings;
@@ -12,15 +7,66 @@ import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.action.ResetSpotTimeFeatureAction;
 import fiji.plugin.trackmate.io.IOUtils;
 import fiji.plugin.trackmate.io.TGMMImporter;
+import ij.IJ;
+import ij.ImageJ;
+import ij.plugin.PlugIn;
 
-public class LoadTGMMAnnotationPlugIn extends NewMamutAnnotationPlugin
+import java.awt.Component;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+import mpicbg.spim.data.SequenceDescription;
+import net.imglib2.realtransform.AffineTransform3D;
+import bdv.SequenceViewsLoader;
+
+public class LoadTGMMAnnotationPlugIn implements PlugIn
 {
 	private static File tgmmFile;
 
+	private static File imageFile;
+
 	@Override
-	protected Model createModel()
+	public void run( final String fileStr )
 	{
 		final Logger logger = Logger.IJ_LOGGER;
+
+		if ( null != fileStr && fileStr.length() > 0 )
+		{
+			// Skip dialog
+			imageFile = new File( fileStr );
+			if ( imageFile.isDirectory() )
+			{
+				imageFile = IOUtils.askForFileForLoading( imageFile, "Open a MaMuT xml file", IJ.getInstance(), logger );
+				if ( null == imageFile ) { return; }
+			}
+			if ( !imageFile.exists() )
+			{
+				IJ.error( MaMuT.PLUGIN_NAME + " v" + MaMuT.PLUGIN_VERSION, "Cannot find image file " + fileStr );
+				return;
+			}
+			if ( !imageFile.canRead() )
+			{
+				IJ.error( MaMuT.PLUGIN_NAME + " v" + MaMuT.PLUGIN_VERSION, "Cannot read image file " + fileStr );
+				return;
+			}
+
+		}
+		else
+		{
+
+			if ( null == imageFile )
+			{
+				final File folder = new File( System.getProperty( "user.dir" ) ).getParentFile().getParentFile();
+				imageFile = new File( folder.getPath() + File.separator + "data.xml" );
+			}
+			imageFile = IOUtils.askForFileForLoading( imageFile, "Open a hdf5/xml file", IJ.getInstance(), logger );
+			if ( null == imageFile ) { return; }
+		}
 
 		if ( null == tgmmFile )
 		{
@@ -28,9 +74,21 @@ public class LoadTGMMAnnotationPlugIn extends NewMamutAnnotationPlugin
 			tgmmFile = new File( folder.getPath() + File.separator + "data.xml" );
 		}
 		tgmmFile = IOUtils.askForFolder( tgmmFile, "Open a TGMM /xml file", IJ.getInstance(), logger );
-		if ( null == tgmmFile ) { return new Model(); }
+		if ( null == tgmmFile ) { return; }
 
-		final TGMMImporter importer = new TGMMImporter( tgmmFile );
+
+		final Model model = createModel( tgmmFile, imageFile );
+		final SourceSettings settings = createSettings();
+		new MaMuT( imageFile, model, settings );
+	}
+
+	protected Model createModel( final File tgmmFile, final File imageFile )
+	{
+		final Logger logger = Logger.IJ_LOGGER;
+
+		final List< AffineTransform3D > transforms = pickTransform( imageFile );
+
+		final TGMMImporter importer = new TGMMImporter( tgmmFile, transforms );
 		if ( !importer.checkInput() || !importer.process() )
 		{
 			logger.error( importer.getErrorMessage() );
@@ -48,6 +106,48 @@ public class LoadTGMMAnnotationPlugIn extends NewMamutAnnotationPlugin
 		action.execute( trackmate );
 
 		return model;
+	}
+
+	protected SourceSettings createSettings()
+	{
+		return new SourceSettings();
+	}
+
+	protected List< AffineTransform3D > pickTransform( final File imageFile )
+	{
+		final Logger logger = Logger.IJ_LOGGER;
+		SequenceViewsLoader loader;
+		try
+		{
+			loader = new SequenceViewsLoader( imageFile.getAbsolutePath() );
+		}
+		catch ( final Exception e )
+		{
+			logger.error( "Problem reading the tranforms in image data file:\n" + e.getMessage() + "\n" );
+			return null;
+		}
+
+		final SequenceDescription seq = loader.getSequenceDescription();
+		final int numViewSetups = seq.numViewSetups();
+		final String[] angles = new String[ numViewSetups ];
+		for ( int setup = 0; setup < numViewSetups; setup++ )
+		{
+			angles[ setup ] = "angle " + seq.setups.get( setup ).getAngle();
+		}
+
+		final Component frame = IJ.getInstance();
+		final Icon icon = MaMuT.MAMUT_ICON;
+		final String s = ( String ) JOptionPane.showInputDialog( frame, "Select the view that was used by the TGMM:", "Pick raw source ", JOptionPane.PLAIN_MESSAGE, icon, angles, angles[ 0 ] );
+		if ( s == null || s.length() == 0 ) { return null; }
+
+		final int setupID = Arrays.asList( angles ).indexOf( s );
+		final List< AffineTransform3D > transforms = new ArrayList< AffineTransform3D >( seq.numTimepoints() );
+		for ( int t = 0; t < seq.numTimepoints(); t++ )
+		{
+			transforms.add( loader.getView( t, setupID ).getModel() );
+		}
+
+		return transforms;
 	}
 
 	public static void main( final String[] args )
