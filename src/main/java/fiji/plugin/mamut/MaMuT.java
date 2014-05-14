13cd3e349c50c8593267509e4571e7877bea6acf
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-05-14 17:16:29 +0200

    More of these double to float cast problems.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/mamut/MaMuT.java
+++ src/main/java/fiji/plugin/mamut/MaMuT.java
@@ -550,7 +550,7 @@ public class MaMuT implements ModelChangeListener
 		displaySettings.put( KEY_HIGHLIGHT_COLOR, DEFAULT_HIGHLIGHT_COLOR );
 		displaySettings.put( KEY_SPOTS_VISIBLE, true );
 		displaySettings.put( KEY_DISPLAY_SPOT_NAMES, false );
-		displaySettings.put( KEY_SPOT_RADIUS_RATIO, 1.0f );
+		displaySettings.put( KEY_SPOT_RADIUS_RATIO, 1.0d );
 		displaySettings.put( KEY_TRACKS_VISIBLE, true );
 		displaySettings.put( KEY_TRACK_DISPLAY_MODE, DEFAULT_TRACK_DISPLAY_MODE );
 		displaySettings.put( KEY_TRACK_DISPLAY_DEPTH, DEFAULT_TRACK_DISPLAY_DEPTH );
