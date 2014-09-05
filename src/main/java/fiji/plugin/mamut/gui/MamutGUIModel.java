99ba348f2bd3acd68f3d18464dbf9588cbacb1d6
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-09-05 10:43:52 +0200

    Change the default parameters for semi-auto tracking.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/mamut/gui/MamutGUIModel.java
+++ src/main/java/fiji/plugin/mamut/gui/MamutGUIModel.java
@@ -7,13 +7,13 @@ public class MamutGUIModel extends TrackMateGUIModel {
 	/**
 	 * How close must be the new spot found to be accepted, in radius units.
 	 */
-	public double distanceTolerance = 1.1;
+	public double distanceTolerance = 1.5;
 
 	/**
 	 * The fraction of the initial quality above which we keep new spots. The
 	 * highest, the more intolerant.
 	 */
-	public double qualityThreshold = 0.2;
+	public double qualityThreshold = 0d;
 
 	/**
 	 * By how much we move in time when calling the step in time action.
