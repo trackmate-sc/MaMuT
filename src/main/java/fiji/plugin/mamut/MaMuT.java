b6aee8d3490a626103c22f4d46b98e11c976dd2b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-10-20 17:27:37 +0200

    Bump to next development cycle.
    v0.16.2-SNAPSHOT
    and depend on pom-fiji 16.1.0.
    
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/mamut/MaMuT.java
+++ src/main/java/fiji/plugin/mamut/MaMuT.java
@@ -129,7 +129,7 @@ public class MaMuT implements ModelChangeListener
 
 	public static final String PLUGIN_NAME = "MaMuT";
 
-	public static final String PLUGIN_VERSION = "0.16.1";
+	public static final String PLUGIN_VERSION = "0.16.2-SNAPSHOT";
 
 	private static final double DEFAULT_RADIUS = 10;
 
