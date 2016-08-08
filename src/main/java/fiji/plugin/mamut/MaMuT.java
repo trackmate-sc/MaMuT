1a19cd2e5d7253fc16530410a286d17b7328cbbe
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2016-08-08 17:38:31 +0200

    Bimp to next development cycle.
    v0.25.2-SNAPSHOT

--- src/main/java/fiji/plugin/mamut/MaMuT.java
+++ src/main/java/fiji/plugin/mamut/MaMuT.java
@@ -136,7 +136,7 @@ public class MaMuT implements ModelChangeListener
 
 	public static final String PLUGIN_NAME = "MaMuT";
 
-	public static final String PLUGIN_VERSION = "0.25.1";
+	public static final String PLUGIN_VERSION = "0.25.2-SNAPSHOT";
 
 	private static final double DEFAULT_RADIUS = 10;
 
