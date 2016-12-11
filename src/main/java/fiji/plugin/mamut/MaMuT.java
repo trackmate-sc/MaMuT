e05fb45c9fe5560b2176d6b2202eb4bfef064f86
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-12-11 17:07:37 +0100

    Release v.0.27.0
    - Have MaMuT properly works with 2D datasets.

--- src/main/java/fiji/plugin/mamut/MaMuT.java
+++ src/main/java/fiji/plugin/mamut/MaMuT.java
@@ -141,7 +141,7 @@ public class MaMuT implements ModelChangeListener
 
 	public static final String PLUGIN_NAME = "MaMuT";
 
-	public static final String PLUGIN_VERSION = "0.26.1-SNAPSHOT";
+	public static final String PLUGIN_VERSION = "0.27.0";
 
 	private static final double DEFAULT_RADIUS = 10;
 
