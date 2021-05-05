0684d101ab20c9f7ac4387816c23d8f980c55606
mhdominguez <69379072+mhdominguez@users.noreply.github.com>
2021-05-05 12:25:03 -0700

    TGMMImporter2::process() does not allow series that begin on frames other than 0 -- even if user sets `tFrom` / `tTo` correctly.  Following these changes, importer works correctly.  Additional changes were made to ImportTGMMAnnotationPlugin_.java to pass list of timepoints for which affine registration models are available, so those can be properly matched with the incoming TGMM data.
    	modified:   src/main/java/fiji/plugin/mamut/ImportTGMMAnnotationPlugin_.java
    	modified:   src/main/java/fiji/plugin/mamut/io/TGMMImporter2.java

--- src/main/java/fiji/plugin/mamut/ImportTGMMAnnotationPlugin_.java
+++ src/main/java/fiji/plugin/mamut/ImportTGMMAnnotationPlugin_.java
@@ -424,12 +424,13 @@ public class ImportTGMMAnnotationPlugin_ implements PlugIn
 		final SequenceDescriptionMinimal seq = spimData.getSequenceDescription();
 		final ViewRegistrations regs = spimData.getViewRegistrations();
 		final List< AffineTransform3D > transforms = new ArrayList<>( seq.getTimePoints().size() );
-		for ( final TimePoint t : seq.getTimePoints().getTimePointsOrdered() )
+		final List< TimePoint > timepoints = seq.getTimePoints().getTimePointsOrdered();
+		for ( final TimePoint t : timepoints )
 		{
 			transforms.add( regs.getViewRegistration( t.getId(), setupID ).getModel() );
 		}
 
-		final TGMMImporter2 importer = new TGMMImporter2( tgmmFolder, transforms, TGMMImporter2.DEFAULT_PATTERN, logger, interval, tFrom, tTo );
+		final TGMMImporter2 importer = new TGMMImporter2( tgmmFolder, transforms, timepoints, TGMMImporter2.DEFAULT_PATTERN, logger, interval, tFrom, tTo );
 		if ( !importer.checkInput() || !importer.process() )
 		{
 			logger.error( importer.getErrorMessage() );
