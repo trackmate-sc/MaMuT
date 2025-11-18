197b733b791553064aca0789c716b7776006e705
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-11-18 16:39:29 +0100

    Fix deprecation of Views.iterable

--- src/main/java/fiji/plugin/mamut/util/SourceSpotImageUpdater.java
+++ src/main/java/fiji/plugin/mamut/util/SourceSpotImageUpdater.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -128,8 +128,8 @@ public class SourceSpotImageUpdater< T extends RealType< T > > extends SpotImage
 					final byte[] imgData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 					final ArrayImg< UnsignedByteType, ByteArray > target = ArrayImgs.unsignedBytes( imgData, width, height );
 
-					final double minValue = Min.findMin(Views.iterable(crop)).get().getRealDouble();
-					final double maxValue = Max.findMax(Views.iterable(crop)).get().getRealDouble();
+					final double minValue = Min.findMin( crop ).get().getRealDouble();
+					final double maxValue = Max.findMax( crop ).get().getRealDouble();
 					final RealUnsignedByteConverter< T > converter = new RealUnsignedByteConverter<>( minValue, maxValue );
 
 					new IterableIntervalProjector2D<>( 0, 1, crop, target, converter ).map();
